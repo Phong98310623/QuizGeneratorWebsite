7217bbc36f8bdbc6fbe5e57e92449216efbcf7a5
Phong98310623 <129152384+Phong98310623@users.noreply.github.com>
2026-02-10 15:33:13 +0700

    Additional fix

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const { USER_STATUS, USER_ROLES } = require("../shares/constants/userRolesAndStatus.js");
 
 const createUser = async (data) => {
   const exists = await User.findOne({ email: data.email });
@@ -28,9 +29,56 @@ const deleteUser = async (id) => {
   return user;
 };
 
+//This function changes user status to inactive after 3 days of inactivity
+const deactivateInactiveUsers = async () => {
+  const threeDaysAgo = new Date();
+  threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+  await User.updateMany(
+    { updatedAt: { $lt: threeDaysAgo }, status: USER_STATUS.ACTIVE },
+    { status: USER_STATUS.INACTIVE }
+  );
+};
+
+//This function promotes user to admin
+const promoteToAdmin = async (id) => {
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (user.role === USER_ROLES.ADMIN) {
+    throw new Error("User is already an admin");
+  }
+
+  user.role = USER_ROLES.ADMIN;
+  await user.save();
+  return user;
+}
+
+//This function ban user
+const banUser = async (id) => {
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (user.status === USER_STATUS.BANNED) {
+    throw new Error("User is already banned");
+  }
+
+  user.status = USER_STATUS.BANNED;
+  await user.save();
+  return user;
+};
+
 module.exports = {
   createUser,
   getAllUsers,
   getUserById,
-  deleteUser
+  deleteUser,
+  deactivateInactiveUsers,
+  promoteToAdmin,
+  banUser
 };
